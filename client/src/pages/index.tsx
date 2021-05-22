d1a190014e818da25405ed55c32fe3e21b136c20
Annette Lau <annette1@ualberta.ca>
2021-05-21 18:47:19 -0600

    whole page linked to auth, styling

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,27 +2,23 @@ import React from "react";
 import { useAuthUser, withAuthUser, withAuthUserTokenSSR } from "next-firebase-auth";
 import Header from "../components/Header";
 import LinearBackground from "../components/LinearBackground";
-import DemoPageLinks from "../components/DemoPageLinks";
+import Head from "../public/head.svg";
 import Button from "../components/Button";
-
-const styles = {
-  content: {
-    padding: 32,
-  },
-  infoTextContainer: {
-    marginBottom: 32,
-  },
-};
 
 const Demo = () => {
   const AuthUser = useAuthUser();
   return (
     <LinearBackground colours={["from-medGreen", "to-yellow"]}>
-      {/* <Header email={AuthUser.email} signOut={AuthUser.signOut} /> */}
-      <div className="justify-items-center">
-        <p className="text-sans text-6xl text-center text-yellow">Study Planner</p>
-        <Button href="/auth">Sign in</Button>
-      </div>
+      <a href="/auth">
+        {/* <Header email={AuthUser.email} signOut={AuthUser.signOut} /> */}
+        <div className="flex flex-col content-center h-screen">
+          <p className="py-12 text-sans text-6xl text-center text-yellow">Study Planner</p>
+          <div className="flex justify-center">
+            <Head />
+          </div>
+          <p className="text-center text-medGreen">Click anywhere to start.</p>
+        </div>
+      </a>
     </LinearBackground>
   );
 };
