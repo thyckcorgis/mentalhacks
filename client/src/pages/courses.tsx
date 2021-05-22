2c9aadfcd52e84f37159ee6340c582ae83d13f16
Annette Lau <annette1@ualberta.ca>
2021-05-21 22:25:35 -0600

    set up courses page

--- client/src/pages/courses.tsx
+++ client/src/pages/courses.tsx
@@ -1,37 +1,19 @@
 import React, { FC, useState } from "react";
 import { useAuthUser, withAuthUser, AuthAction, withAuthUserTokenSSR } from "next-firebase-auth";
 import LinearBackground from "../components/LinearBackground";
+import Header from "../components/Header";
 import Button from "../components/Button";
 
 interface CourseProps {}
 
 const Courses: FC<CourseProps> = ({}) => {
   const AuthUser = useAuthUser();
-  const [currentTab, setCurrentTab] = useState<Tab>(Tab.ALL);
-  const changeTab = (tab: Tab) => {
-    if (tab === currentTab) return;
-    setCurrentTab(tab);
-  };
   return (
     <LinearBackground colours={["from-darkGreen", "to-medGreen"]}>
       <Header email={AuthUser.email} signOut={AuthUser.signOut} />
       <div className="flex flex-col h-4/5 content-center">
         <p className="py-2 text-sans text-4xl text-center text-yellow">Welcome, {name}</p>
-        <div className="mx-auto">
-          {tabs.map(([name, tab]) => (
-            <button
-              className={`rounded-t-lg px-10 py-2 bg-medGreen opacity-70 focus:outline-none focus:opacity-100 ${
-                tab === currentTab ? "opacity-100" : "opacity-70"
-              }`}
-              onClick={() => changeTab(tab)}
-            >
-              {name}
-            </button>
-          ))}
-        </div>
-        <div className="h-full w-4/5 bg-gradient-to-b from-medGreen rounded-lg mx-auto ">
-          <CurrentTab currentTab={currentTab} />
-        </div>
+        <div className="h-full w-4/5 bg-gradient-to-b from-medGreen rounded-lg mx-auto ">pee</div>
       </div>
     </LinearBackground>
   );
@@ -49,4 +31,4 @@ export const getServerSideProps = withAuthUserTokenSSR({
 
 export default withAuthUser({
   whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
-})(Dashboard);
+})(Courses);
