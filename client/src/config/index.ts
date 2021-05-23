6f33396007a5cede161f4fe156d276f1f54b2728
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-23 02:22:58 +0000

    add scopes

---
+++ client/src/config/index.ts
@@ -0,0 +1,5 @@
+export const scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/calendar.app.created",
+  "https://www.googleapis.com/auth/calendar.events.freebusy",
+];
